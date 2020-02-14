5df0958e20c474795515f18901278dc6aff5c0b5
zdhwg <46347570+zdhwg@users.noreply.github.com>
2020-02-14 15:33:38 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -22,7 +22,6 @@ function loadWidget(config) {
 				<span class="fa fa-lg fa-user-circle"></span>
 				<span class="fa fa-lg fa-street-view"></span>
 				<span class="fa fa-lg fa-camera-retro"></span>
-				<span class="fa fa-lg fa-info-circle"></span>
 				<span class="fa fa-lg fa-times"></span>
 			</div>
 		</div>`);
